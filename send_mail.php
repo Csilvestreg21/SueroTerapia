50f764e6bd8a035eab39dc02dde866d87f675327
AdrianaOrozcoA <qacatavar@gmail.com>
2024-10-08 19:02:22 -0500

    Descripción de los cambios realizados

--- send_mail.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (mail($recipient, $subject, $email_content, $email_headers)) {
-    echo "Gracias por tu mensaje. ¡Nos pondremos en contacto contigo pronto!";
-} else {
-    echo "Oops! Algo salió mal y no pudimos enviar tu mensaje.";
-}
-?>
